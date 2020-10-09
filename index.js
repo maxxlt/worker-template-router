ac517c8177e966ce030fd785057574c8fee5ac84
maxthelearner <din.chan@mavs.uta.edu>
2020-10-09 01:54:13 -0500

    Initial commit

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 const Router = require('./router')
 
-/**
- * Example of how router can be used in an application
- *  */
 addEventListener('fetch', event => {
     event.respondWith(handleRequest(event.request))
 })
 
 function handler(request) {
     const init = {
-        headers: { 'content-type': 'application/json' },
+        headers: {
+            'content-type': 'application/json'
+        },
     }
-    const body = JSON.stringify({ some: 'json' })
+    var text = '[' +
+        '{ "name":"John Doe" , "url":"https://www.cloudflare.com/" },' +
+        '{ "name":"Max Chan" , "url":"https://1.1.1.1/" },' +
+        '{ "name":"Peter Jackson" , "url":"https://google.com" } ]';
+
+    const list = JSON.parse(text);
+    const body = JSON.stringify(list)
     return new Response(body, init)
 }
 
 async function handleRequest(request) {
     const r = new Router()
-    // Replace with the appropriate paths and handlers
-    r.get('.*/bar', () => new Response('responding for /bar'))
-    r.get('.*/foo', request => handler(request))
-    r.post('.*/foo.*', request => handler(request))
-    r.get('/demos/router/foo', request => fetch(request)) // return the response from the origin
-
-    r.get('/', () => new Response('Hello worker!')) // return a default message for the root route
+    r.get('/', () => new Response('Hello! Go to /links to glance on our amazing looking JSON!'))
+    r.get('.*/links', request => handler(request))
 
     const resp = await r.route(request)
     return resp
